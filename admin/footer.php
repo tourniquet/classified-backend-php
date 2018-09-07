aefd6c4cf148bb186a091b73d39e9fdf130d53ad
Ion Prodan <admyn3d@gmail.com>
2018-09-07 20:16:59 +0100

    Moving footer and header to admin folder

---
+++ admin/footer.php
@@ -0,0 +1,19 @@
+  <ul class="admin-footer-list">
+    <li class="admin-footer-list-item">
+      Thank you for using CLASSIFIED
+    </li>
+
+    <li class="admin-footer-list-item">
+      Documentation
+    </li>
+
+    <li class="admin-footer-list-item">
+      Forums
+    </li>
+
+    <li class="admin-footer-list-item">
+      Feedback
+    </li>
+  </ul>
+</body>
+</html>
