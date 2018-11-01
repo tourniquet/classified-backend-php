6636675b381f7f8ca16d074d8df81aac0647dfdd
tourniquet <admyn3d@gmail.com>
2018-11-01 17:40:44 +0000

    improving admin panel

--- admin/index.php
+++ admin/index.php
@@ -53,19 +53,26 @@
 
   echo '<ul class="items-list">';
   while ($row = mysqli_fetch_array($data)) {
-    if ($row['enabled'] == 1) {
-      echo '<li class="enabled">' . $row['title'] .
-          '<a href="remove.php?id=' . $row['id'] . '"> Remove</a>&nbsp;
-          <a href="disable.php?id=' . $row['id'] . '">Disable</a>
-          <a href="renew.php?id=' . $row['id'] . '">Renew</a>
-        </li>';
-    } else {
-      echo '<li class="disabled">' . $row['title'] .
-          '<a href="remove.php?id=' . $row['id'] . '"> Remove</a>&nbsp;
-          <a href="enable.php?id=' . $row['id'] . '">Enable</a>&nbsp;
-          <a href="renew.php?id=' . $row['id'] . '">Renew</a>
-        </li>';
-    }
+    // TODO: sprintf()
+
+    $item_state = $row['enabled'] == 1
+      ? array(
+        'class_name' => 'enabled',
+        'href' => 'enable.php?id=' . $row['id'],
+        'title' => 'Enable'
+      )
+      : array(
+        'class_name' => 'disabled',
+        'href' => 'disable.php?id=' . $row['id'],
+        'title' => 'Disable'
+      );
+
+
+    echo '<li class="' . $item_state['class_name']  . '">' . $row['title'] .
+            '<a href="remove.php?id=' . $row['id'] . '"> Remove</a>&nbsp;
+            <a href="' . $item_state['href'] . '">' . $item_state['title'] .'</a>
+            <a href="renew.php?id=' . $row['id'] . '">Renew</a>
+          </li>';
   }
   echo '</ul>';
 
