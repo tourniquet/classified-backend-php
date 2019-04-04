4137cb9071ee885c737363ccc14f34acd4eb0809
tourniquet <admyn3d@gmail.com>
2019-04-04 21:07:29 +0100

    Subcategory is shown for every item on index page

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 <?php
   require_once('dbc.php');
 
-  $query = "SELECT * FROM cls_ads ORDER BY published DESC";
+  $query = "SELECT ads.*, sub.title AS subcategory, cat.title AS category
+    FROM cls_ads AS ads
+    INNER JOIN cls_categories AS sub ON ads.subcategory_id = sub.id
+    INNER JOIN cls_categories AS cat ON sub.parent_id = cat.id
+    ORDER BY published DESC
+  ";
   $data = mysqli_query($dbc, $query);
 
   $res = [];
@@ -17,6 +22,7 @@
   header('Access-Control-Allow-Origin: *', false);
   header('Content-type: application/json', false);
   header('HTTP/1.1 200 OK');
+
   echo json_encode($res);
 
   mysqli_close($dbc);
