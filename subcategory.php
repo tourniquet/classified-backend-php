0af90f4d13bb07971e91b837344bc4fa40426691
tourniquet <admyn3d@gmail.com>
2019-04-11 22:47:05 +0100

    Minor changes in SQL query

--- subcategory.php
+++ subcategory.php
@@ -4,7 +4,7 @@
   $query = "SELECT ads.*, sub.title AS subcategory
     FROM cls_ads AS ads
     INNER JOIN cls_categories AS sub ON ads.subcategory_id = sub.id
-    WHERE sub.title = '" . $_GET['url'] . "'";
+    WHERE sub.title = '" . $_GET['subcategory'] . "'";
   $data = mysqli_query($dbc, $query) or die('subcategory.php mysql error');
 
   $res = [];
