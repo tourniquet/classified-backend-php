b22fe36367ddf0ccf339a48321f493094a326a38
Ion Prodan <admyn3d@gmail.com>
2019-07-07 22:55:54 +0100

    Basic SQL query for pagination on Region page

--- region.php
+++ region.php
@@ -2,24 +2,40 @@
   require_once('dbc.php');
 
   $region = $_GET['region'];
+  $page_number = $_GET['page'];
+  $items_per_page = 10;
+  $offset = ($page_number - 1) * $items_per_page;
 
   $query = "SELECT ads.*, region.title AS region
     FROM cls_ads AS ads
     INNER JOIN cls_regions AS region ON ads.region_id = region.id
     WHERE region.title = '$region'
-  ";
+    ORDER BY published DESC
+    LIMIT $items_per_page OFFSET $offset";
   $data = mysqli_query($dbc, $query) or die('region.php mysql error');
 
-  $res = [];
+  $items = [];
   while ($i = mysqli_fetch_assoc($data)) {
-    $res[] = $i;
+    $items[] = $i;
   }
+
+  $query = "SELECT COUNT(*) AS total, region.title AS region
+    FROM cls_ads AS ads
+    INNER JOIN cls_regions AS region ON ads.region_id = region.id
+    WHERE region.title = '$region'";
+  $res = mysqli_query($dbc, $query);
+  $total = mysqli_fetch_row($res);
+
+  mysqli_close($dbc);
 
   header('Access-Control-Allow-Origin: *', false);
   header('Content-type: application/json', false);
   header('HTTP/1.1 200 OK');
 
-  echo json_encode($res);
+  $data = (object)[];
+  $data->items = $items;
+  $data->page = $page_number;
+  $data->total = $total[0];
 
-  mysqli_close($dbc);
+  echo json_encode($data);
 ?>
