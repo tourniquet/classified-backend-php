fd46952f25d83a2633264bbbe0e8b11924a6cccb
Ion Prodan <admyn3d@gmail.com>
2019-07-16 17:30:34 +0100

    Added basic pagination to Currencies manager screen

--- admin/currencies.php
+++ admin/currencies.php
@@ -49,6 +49,39 @@
           </div>
         ';
       }
+      $query = "SELECT COUNT(*) AS total FROM cls_currencies";
+      $res = mysqli_query($dbc, $query);
+      $total_items = mysqli_fetch_row($res);
+      $total_pages = ceil($total_items[0] / 10);
+
+      mysqli_close($dbc);
+
+      $pages = [];
+      for ($i = 0; $i < $total_pages; $i++) {
+        $pages[] = $i + 1;
+      }
+
+      echo '<ul class="pagination">';
+      if ($page == 1) {
+        echo '<li class="prev-button disabled">Prev</li>';
+      } else {
+        echo '<li class="prev-button"><a href="currencies.php?page=' . ($page - 1) . '">Prev</a></li>';
+      }
+
+      for ($i = 0; $i < count($pages); $i++) {
+        if ($page == $pages[$i]) {
+          echo '<li><a class="page active" href="currencies.php?page=' . $pages[$i] . '">' . $pages[$i] . '</a></li>';
+        } else {
+          echo '<li><a class="page" href="currencies.php?page=' . $pages[$i] . '">' . $pages[$i] . '</a></li>';
+        }
+      }
+
+      if ($page >= count($pages)) {
+        echo '<li class="next-button disabled">Next</li>';
+      } else {
+        echo '<li class="next-button"><a href="currencies.php?page=' . ($page + 1) . '">Next</a></li>';
+      }
+      echo '</ul>';
     ?>
   </main>
 </div>
