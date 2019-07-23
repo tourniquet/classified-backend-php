1d752aa2f4b82645225753c9b4f7d9a1e2e29d4b
Ion Prodan <admyn3d@gmail.com>
2019-07-23 17:55:38 +0100

    Refactored listing loop, fixed minor styles issues

--- admin/index.php
+++ admin/index.php
@@ -79,42 +79,59 @@
       //   echo '<a href="../login.php" style="color: red;">Login</a>';
       // }
 
-        echo '<ul class="items-list">';
-        while ($row = mysqli_fetch_array($data)) {
-          // TODO: sprintf()
-
-          $item_state = $row['enabled'] == 1
-            ? array(
-              'class_name' => 'enabled',
-              'href' => 'disable.php?id=' . $row['id'],
-              'state' => 'Disable'
-            )
-            : array(
-              'class_name' => 'disabled',
-              'href' => 'enable.php?id=' . $row['id'],
-              'state' => 'Enable'
-            );
-
-          echo '<li class="' . $item_state['class_name'] . '">' . '
-                  <a href="#" class="item-title">' . $row['title'] . '</a>
-                  <a href="#" class="subcategory">' . $row['subcategory'] . '</a>
-                  <span class="">' . date_format(date_create($row['published']), 'd F, Y') . '</span>
-                  <div class="action-icons">
-                    <a href="#" class="url-icon"></a>
-                    <a href="#" class="edit-icon"></a>
-                    <a href="' . $item_state['href'] . '" class="enable-icon"></a> <!-- $item_state[state] -->
-                    <a href="renew.php?id=' . $row['id'] . '" class="renew-icon"></a>
-                    <a href="#" class="disable-icon"></a>
-                    <a href="#" class="spam-icon"></a>
-                    <a href="remove.php?id=' . $row['id'] . '" class="remove-icon"></a>
-                  </div>
-                </li>';
-        }
-        echo '</ul>';
+      echo '<ul class="items-list">';
+      while ($row = mysqli_fetch_array($data)) {
+        // TODO: sprintf()
+        $item_state = $row['enabled'] == 1
+          ? array(
+            'class_name' => 'enabled',
             'href' => 'disable-item.php?id=' . $row['id'],
+            'state' => 'Disable'
+          )
+          : array(
+            'class_name' => 'disabled',
             'href' => 'enable-item.php?id=' . $row['id'],
+            'state' => 'Enable'
+          );
+
+          echo "<li class='{$item_state['class_name']}'>
+            <span class='check-item'>
+              <input type='checkbox'>
+            </span>
+            <span class='item-id'>
+              {$row['id']}
+            </span>
+            <a href='" . FRONT_SIDE . "/item/" . $row['url'] . "' class='item-title' target='_blank'>
+              {$row['title']}
+            </a>
+            <a href='#' class='subcategory'>
+              {$row['subcategory']}
+            </a>
+            <span class='item-posted-date'>" . date_format(date_create($row['published']), 'd F, Y') . "</span>
+            <div class='action-icons'>
+              <a href='" . FRONT_SIDE . "/item/" . $row['url'] . "' class='item-title' target='_blank'>
+                <i class='icon ion-md-link'></i>
+              </a>
+              <a href='#' class='edit-icon'>
+                <i class='icon ion-md-create'></i>
+              </a>
+              <a href='{$item_state['href']}' class='enable-icon'></a>
               <a href='renew-item.php?id={$row['id']}' class='renew-icon'>
+                <i class='icon ion-md-refresh'></i>
+              </a>
+              <a href='#' class='disable-icon'>
+                <i class='icon ion-md-eye-off'></i>
+              </a>
+              <a href='#' class='spam-icon'>
+                <i class='icon ion-md-flame'></i>
+              </a>
               <a href='remove-item.php?id={$row['id']}' class='remove-icon' onclick='return confirm('Are you sure?')'>
+                <i class='icon ion-md-trash'></i>
+              </a>
+            </div>
+          </li>";
+      }
+      echo '</ul>';
 
       $query = "SELECT COUNT(*) AS total FROM cls_ads";
       $res = mysqli_query($dbc, $query);
