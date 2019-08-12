cdb0c5ccd8684b213c191f2f9bae98cb29e931b4
Ion Prodan <admyn3d@gmail.com>
2019-08-12 10:05:47 +0100

    Redirect to index page after removing a category

--- admin/categories/remove-categories.php
+++ admin/categories/remove-categories.php
@@ -1,27 +1,16 @@
 <?php
   require_once('../../private/initialize.php');
-  include(SHARED_PATH . '/head.php');
+  require_once('../../dbc.php');
+
+  if (isset($_POST['submit']) && isset($_POST)) {
+    $items = implode(',', $_POST['items']);
+
+    $remove_category_query = "DELETE
+      FROM cls_categories
+      WHERE id IN ($items)";
+    mysqli_query($dbc, $remove_category_query);
+    mysqli_close($dbc);
+  }
+
+  redirect_to('index.php?page=1');
 ?>
-
-<div class="admin-panel">
-  <?php
-    include(SHARED_PATH . '/header.php');
-    require_once('../../dbc.php');
-
-    if (isset($_POST['submit']) && isset($_POST)) {
-      $items = implode(',', $_POST['items']);
-
-      $remove_category_query = "DELETE
-        FROM cls_categories
-        WHERE id IN ($items)";
-      mysqli_query($dbc, $remove_category_query);
-      mysqli_close($dbc);
-    }
-  ?>
-
-  <div>
-    <a href="index.php?page=1"><<< Back</a>
-  </div>
-
-  <?php include(SHARED_PATH . '/footer.php'); ?>
-</div>
