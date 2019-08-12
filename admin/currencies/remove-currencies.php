89c86ed2a73400d7f2f4ba996ebb437b1b7fbab9
Ion Prodan <admyn3d@gmail.com>
2019-08-12 10:45:26 +0100

    Remove currencies redirect to index (currencies) page

--- admin/currencies/remove-currencies.php
+++ admin/currencies/remove-currencies.php
@@ -1,27 +1,16 @@
 <?php
   require_once('../../private/initialize.php');
-  include(SHARED_PATH . '/head.php');
+  require_once('../../dbc.php');
+
+  if (isset($_POST['submit']) && isset($_POST)) {
+    $items = implode(',', $_POST['items']);
+
+    $remove_currencies_query = "DELETE
+      FROM cls_currencies
+      WHERE id IN ($items)";
+    mysqli_query($dbc, $remove_currencies_query);
+    mysqli_close($dbc);
+  }
+
+  redirect_to('index.php?page=1');
 ?>
-
-<div class="admin-panel">
-  <?php
-    include(SHARED_PATH . '/header.php');
-    require_once('../../dbc.php');
-
-    if (isset($_POST['submit']) && isset($_POST)) {
-      $items = implode(',', $_POST['items']);
-
-      $remove_currencies_query = "DELETE
-        FROM cls_currencies
-        WHERE id IN ($items)";
-      mysqli_query($dbc, $remove_currencies_query);
-      mysqli_close($dbc);
-    }
-  ?>
-
-  <div>
-    <a href="index.php?page=1"><<< Back</a>
-  </div>
-
-  <?php include(SHARED_PATH . '/footer.php'); ?>
-</div>
