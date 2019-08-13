12167a36d98e17a5e688de9c915c0d44f8a6b944
Ion Prodan <admyn3d@gmail.com>
2019-08-13 11:50:51 +0100

    Removed login functionality from admin index page

--- admin/index.php
+++ admin/index.php
@@ -1,36 +1,3 @@
-  <!-- // $error_msg = '';
-
-  // if (!isset($_COOKIE['email'])) {
-    // $home_url = 'http://' . $_SERVER['HTTP_HOST'] . '/classified/backend/login.php';
-    // header('Location: ' . $home_url);
-
-    // if (isset($_POST['submit'])) {
-      // require_once('../dbc.php');
-
-      // $email = mysqli_real_escape_string($dbc, trim($_POST['email']));
-      // $password = mysqli_real_escape_string($dbc, trim($_POST['password']));
-
-      // if (!empty($email) && !empty($password)) {
-        // $query = "SELECT email FROM cls_users WHERE email = '$email' AND password = SHA('$password')";
-        // $data = mysqli_query($dbc, $query);
-
-        // if (mysqli_num_rows($data) == 1) {
-          // $row = mysqli_fetch_array($data);
-          // setcookie('email', $row['email']);
-
-          // $home_url = 'http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']) . '/';
-          // header('Location: ' . $home_url);
-        // }
-        // else {
-        //   $error_msg = 'Sorry, you must enter a valid email and password to log in.';
-        // }
-      // }
-      // else {
-      //   $error_msg = 'Sorry, you must enter your email and password to log in.';
-      // }
-    // }
-  // } -->
-
 <?php
   include_once './head.php';
 ?>
@@ -73,12 +40,6 @@
         // }
 
         // echo json_encode($data);
-
-        // if (isset($_COOKIE['email'])) {
-        //   echo '<a href="../logout.php" style="color: red;">Logout</a>';
-        // } else {
-        //   echo '<a href="../login.php" style="color: red;">Login</a>';
-        // }
 
         echo '<ul class="items-list">';
         while ($row = mysqli_fetch_assoc($data)) {
