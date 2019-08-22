5e43332a471a569740330af1866def7db807d620
Ion Prodan <admyn3d@gmail.com>
2019-08-22 09:53:21 +0100

    User must confirm to remove an image

--- admin/js/remove-image.js
+++ admin/js/remove-image.js
@@ -1,13 +1,14 @@
-/* global XMLHttpRequest, alert */
+/* global XMLHttpRequest, alert, confirm */
 
 function removeImage (image, item) { // eslint-disable-line no-unused-vars
   let xhr = new XMLHttpRequest()
   xhr.open('GET', `../admin/remove-image.php?image=${image}&item=${item}`)
-  xhr.send()
+  if (confirm('Are you sure you want to delete this image?')) {
+    xhr.send()
+  }
 
   xhr.onload = function () {
     if (xhr.status === 200) {
-      alert(`image: ${image}, item: ${item}`)
       document.getElementById(image).remove()
     }
   }
