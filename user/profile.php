c7b6e3e5d8ab41d340b9443f48cbccc5667d5ba3
Ion Prodan <admyn3d@gmail.com>
2019-09-03 14:40:51 +0100

    Moved profile.php to user/ folder. Return Subcategory and thumbnail

--- user/profile.php
+++ user/profile.php
@@ -1,14 +1,16 @@
 <?php
-  require_once('./private/initialize.php');
-  require_once('dbc.php');
+  require_once('../private/initialize.php');
+  require_once('../dbc.php');
 
   $page_number = isset($_GET['page']) ? db_escape($dbc, $_GET['page']) : 1;
   $items_per_page = 10;
   $offset = ($page_number - 1) * $items_per_page;
 
   $id = db_escape($dbc, $_GET['id']);
-  $query = "SELECT *
-    FROM cls_ads
+  $query = "SELECT ads.*, sub.name AS subcategory, cat.name AS category
+    FROM cls_ads AS ads
+    INNER JOIN cls_categories AS sub ON ads.subcategory_id = sub.id
+    INNER JOIN cls_categories AS cat ON sub.parent_id = cat.id
     WHERE user_id = '$id' AND enabled = 1
     ORDER BY published DESC
     LIMIT $items_per_page OFFSET $offset";
@@ -17,6 +19,26 @@
   $items = [];
   while ($item = mysqli_fetch_assoc($user_items)) {
     $items[] = $item;
+  }
+
+    // If an item have at least one image, JSON returned will contain images: value
+  // images query
+  $query = "SELECT *
+    FROM cls_images";
+  $result = mysqli_query($dbc, $query);
+  $images = [];
+  while ($i = mysqli_fetch_assoc($result)) {
+    $images[] = $i;
+  }
+
+  $images_length = sizeof($images);
+  $items_length = sizeof($items);
+  for ($i = 0; $i < $images_length; $i++) {
+    for ($j = 0; $j < $items_length; $j++) {
+      if ($items[$j]['id'] === $images[$i]['ad_id']) {
+        $items[$j]['images'] = $images[$i]['image'];
+      }
+    }
   }
 
   $query = "SELECT COUNT(*) AS total
